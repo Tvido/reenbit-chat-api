d6185deea0ec783024b37654ca6ca7d1882fc878
Volodymyr Viter <mad.tvido@gmail.com>
2022-08-22 13:49:33 +0300

    init project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((error, _, res, __) => {
+  res.status(500).json({ message: error.message });
+});
+
+module.exports = app;
